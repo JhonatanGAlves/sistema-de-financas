d97a1c73945646e1a2048b24019ea32adba08d43
JhonatanGAlves <jhonatangalves96@gmail.com>
2021-10-13 10:48:16 -0300

    Creation of date filtering functions

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,6 +1,6 @@
 import { Item } from "../types/Item"
 
-export const Items: Item[] = [
+export const items: Item[] = [
     { date: new Date(2021, 8, 30), category: 'salary', title: 'Salário Intelie', value: 1800.00 },
     { date: new Date(2021, 9, 3), category: 'salary', title: 'Salário Lwart', value: 650.00 },
     { date: new Date(2021, 9, 3), category: 'rent', title: 'Aluguel', value: 800.00 },
