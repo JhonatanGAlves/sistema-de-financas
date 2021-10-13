0523f5ca307f54420c4d9420e17be9c32b037b55
JhonatanGAlves <jhonatangalves96@gmail.com>
2021-10-13 08:46:42 -0300

    Creating my internal database

---
+++ src/data/categories.ts
@@ -0,0 +1,6 @@
+import { Category } from "../types/Category"
+
+export const categories: Category = {
+    salary: { title: 'Pagamento', color: '#31b300', expense: false },
+    rent: { title: 'Aluguel da Casa', color: '#ffd900', expense: true }
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item"
+
+export const Items: Item[] = [
+    { date: new Date(2021, 8, 30), category: 'salary', title: 'Salário Intelie', value: 1800.00 },
+    { date: new Date(2021, 9, 3), category: 'salary', title: 'Salário Lwart', value: 650.00 },
+    { date: new Date(2021, 9, 3), category: 'rent', title: 'Aluguel', value: 800.00 },
+    { date: new Date(2021, 9, 18), category: 'salary', title: 'Vale', value: 700.00 }
+]
